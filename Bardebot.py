0f7c9dd5c8981dae9d764c25cd21d5b534a8481a
Kevin Rassele <kubstitheking@gmail.com>
2021-09-18 13:20:07 +0000

    Bot connection established

---
+++ Bardebot.py
@@ -0,0 +1,23 @@
+# Bardebot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+        if message.author == client.user:
+            return
+        if'!play' in message.content.lower():
+            await message.channel.send('Works')
+
+client.run(TOKEN)
