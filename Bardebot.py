4501ea5ba145adaf8902770fcb05d1959e83000c
Kevin Rassele <kubstitheking@gmail.com>
2021-09-20 18:15:50 +0000

    refractored code

--- Bardebot.py
+++ Bardebot.py
@@ -2,22 +2,41 @@
 import os
 
 import discord
+import asyncio
+from discord.player import FFmpegAudio
+import pafy
+from discord.ext import commands
+from discord.ext.commands import bot
 from dotenv import load_dotenv
 
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+bot = commands.Bot(command_prefix="+")
 client = discord.Client()
+ffmpeg_options = {
+    'options': '-vn'
+}
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-        if message.author == client.user:
-            return
-        if'!play' in message.content.lower():
-            await message.channel.send('Works')
+@bot.command()
+async def p(ctx):
+    file = pafy.new('')
+    
+    if ctx.author.voice is None or ctx.author.voice.channel is None:
+            return await ctx.send('You need to be in a voice channel to use this command!')
 
-client.run(TOKEN)
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+        vc = await voice_channel.connect()
+    elif ctx.voice_client.channel is ctx.author.voice.channel:
+        await ctx.voice_client.move_to(voice_channel)
+        vc = ctx.voice_client
+
+    async with ctx.typing():
+        audio = file.getbestaudio()
+        source = discord.FFmpegPCMAudio(audio.url, **ffmpeg_options)
+        vc.play(source)
+      
+bot.run(TOKEN)
+
